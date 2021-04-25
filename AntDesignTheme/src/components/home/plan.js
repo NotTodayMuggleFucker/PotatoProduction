f59306963c74aa83e088596fa7b3bf1b07a4d44f
David C <davidkalvo@hotmail.com>
2021-04-24 21:59:46 -0500

    Update

---
+++ AntDesignTheme/src/components/home/plan.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AppPlan() {
+    return (
+       <h2>dsds</h2>
+        );
+    }
+    
+    export default AppPlan;
--- AntDesignTheme/src/views/home.js
+++ AntDesignTheme/src/views/home.js
@@ -3,6 +3,10 @@ import React from 'react';
 import AppHero from '../components/home/hero';
 import AppAbout from '../components/home/about';
 import AppFeature from '../components/home/feature';
+import AppWorks from '../components/home/works';
+import AppFaq from '../components/home/faq';
+import AppPlan from '../components/home/plan';
+
 
 function AppHome() {
     return (
@@ -10,6 +14,9 @@ function AppHome() {
           <AppHero/>
           <AppAbout/>
           <AppFeature/>
+          <AppWorks/>
+          <AppFaq/>
+          <AppPlan/>
         </div>
     );
 }
