cdafdf3341c4a4d594b2fd753022ff9a70c43923
David C <davidkalvo@hotmail.com>
2021-04-29 20:44:00 -0500

    Update

--- AntDesignTheme/src/components/common/header.js
+++ AntDesignTheme/src/components/common/header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo from '../../logo_catapulta.svg';
 import { Anchor, Drawer, Button } from 'antd';
+import { AiOutlineMenu } from "react-icons/ai";
 
 const { Link } = Anchor;
 
@@ -22,7 +23,7 @@ function AppHeader() {
              <div className="logo"/>*/} 
             <a href="http://www.catapultadesign.com">           
             <img src={logo} className="logo" alt="logo"/></a>
-            <div className="mobileHidden>">
+            <div className="mobileHidden">
             <Anchor targetOffset="65">
                     <Link href="#hero" title="Home" />
                     <Link href="#about" title="About" />
@@ -32,27 +33,29 @@ function AppHeader() {
                     <Link href="#pricing" title="Pricing" />
                     <Link href="#contact" title="Contact" />        
                 </Anchor>
-  
+               </div>   
             <div className="mobileVisible"> 
                 <Button type="primary" onClick={showDrawer}>
-                    Menu
+                <AiOutlineMenu className="fas"/>
                 </Button>
                 <Drawer
-                    title="Menu"
                     placement="right"
                     closable={false}
                     onClose={onClose}
                     visible={visible}
                     >
-                    <p>Home</p>
-                    <p>About</p>
-                    <p>Feature</p>
-                    <p>FAQ</p>
-                    <p>Contact</p>
+                    <Anchor targetOffset="65">
+                    <Link href="#hero" title="Home" />
+                    <Link href="#about" title="About" />
+                    <Link href="#features" title="Features" />
+                    <Link href="#works" title="How it Works" />
+                    <Link href="#faq" title="FAQ" />
+                    <Link href="#pricing" title="Pricing" />
+                    <Link href="#contact" title="Contact" />        
+                </Anchor>
                 </Drawer>
-                </div>
              </div>
-          </div>
+           </div>
         </div>
     );
 }
