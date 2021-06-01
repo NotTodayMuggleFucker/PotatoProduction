158c4d5a9d344aa2eda508e2fd44412ab073af2a
David C <davidkalvo@hotmail.com>
2021-06-01 17:19:11 -0500

    Update

--- CatapultaTemplates/src/components/pages/Homepage/Data.js
+++ CatapultaTemplates/src/components/pages/Homepage/Data.js
@@ -5,7 +5,7 @@ export const homeObjOne = {
     lightText: true,
     lightTextDesc: true,
     topLine: 'Exclusive Access',
-    headline: 'Unlimited Transactions with zero fees',
+    headline: 'Catapulta Templates One - Unlimited Transactions with zero fees',
     description:
       'Get access to our exclusive diamond card that allows you to send unlimited transactions without getting charged any fees',
     buttonLabel: 'Get Started',
